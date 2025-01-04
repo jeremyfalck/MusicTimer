5239c2c923159e8ed05c9e5b287f0a41379c1569
jeremyfalck <jeremy.falck@proton.me>
2025-01-04 19:14:31 +0100

    release: 0.10

--- musictimer-app/build.gradle.kts
+++ musictimer-app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jfalck.musictimer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "0.9"
+        versionCode = 10
+        versionName = "0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
